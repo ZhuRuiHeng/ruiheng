8e87ad6230390be0af3c19b323f2f33b2c1e5692
朱瑞恒 <18749830459@163.com>
2017-08-30 19:16:33 +0800

    新增收货地址，选择联系人
    nextAddress:function(){
        // wx.navigateTo({
        //   url: '../myAddress/myAddress'
        // })
        wx.chooseAddress({
          success: function (res) {
            console.log(res.userName)
            console.log(res.postalCode)
            console.log(res.provinceName)
            console.log(res.cityName)
            console.log(res.countyName)
            console.log(res.detailInfo)
            console.log(res.nationalCode)
            console.log(res.telNumber)
          }
        })
      },

--- pages/dingdanInform/dingdanInform.js
+++ pages/dingdanInform/dingdanInform.js
@@ -15,8 +15,20 @@ Page({
     
   },
   nextAddress:function(){
-    wx.navigateTo({
-      url: '../myAddress/myAddress'
+    // wx.navigateTo({
+    //   url: '../myAddress/myAddress'
+    // })
+    wx.chooseAddress({
+      success: function (res) {
+        console.log(res.userName)
+        console.log(res.postalCode)
+        console.log(res.provinceName)
+        console.log(res.cityName)
+        console.log(res.countyName)
+        console.log(res.detailInfo)
+        console.log(res.nationalCode)
+        console.log(res.telNumber)
+      }
     })
   },
   /**
